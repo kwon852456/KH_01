a8123a9a6b3db7aa1f0766ff08889d3cc2c63bc5
kwon <kwon852456@naver.com>
2019-03-30 16:46:07 +0900

    KH_From the korea

---
+++ KH_01.hpp
@@ -0,0 +1,12 @@
+﻿#ifndef KH_01_HPP
+#define KH_01_HPP
+
+template<typename Func,typename... Args>
+inline auto curry(Func func, Args... args){
+
+    return [=]( auto... secondParam){   return func(args..., secondParam...);  };
+
+}
+
+
+#endif // KH_01_HPP
